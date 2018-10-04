3d773c75685da37746a7108a7c0fb7bfd1c38dfc
Etienne <etienne@kn0x.io>
2018-10-04 15:05:36 -0400

    Add basic includes and function prototypes

---
+++ lab1/src/charDriver.c
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  charDriver.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/10/2018 15:00:26
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Etienne B.-Pilon BOUE07079001
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <uapi/asm-generic/errno-base.h>
+#include <linux/fcntl.h>
+#include <linux/wait.h>
+#include <linux/spinlock.h>
+#include <linux/device.h>
+#include <asm/atomic.h>
+#include <asm/uaccess.h>
+
+// -- Infos --
+MODULE_AUTHOR("Etienne B.-Pilon");
+MODULE_LICENSE("Dual BSD/GPL");
+
+// -- Prototypes --
+static int __init cd_init(void);
+static void __exit cd_exit(void);
+static int cd_open(struct inode *inode, struct file *flip);
+static int cd_release(struct inode *inode, struct file *flip);
+static ssize_t cd_read(struct file *flip, char __user *ubuf, size_t count, loff_t *f_ops);
+static ssize_t cd_write(struct file *flip, const char __user *ubuf, size_t count, loff_t *f_ops);
