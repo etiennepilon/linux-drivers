07954c688be451fd83a5d45ad2246cafc403f318
Etienne <etienne.b.pilon@gmail.com>
2018-11-23 11:36:49 -0500

    Add probe and disconnect functions

--- lab2/src/usb_cam.c
+++ lab2/src/usb_cam.c
@@ -20,10 +20,15 @@
 #include <linux/usb.h>
 #include <linux/completion.h>
 #include "usb_cam.h"
+#include "usbvideo.h"
+#include "dht_data.h"
 
 // -- Module Basic Information --
 MODULE_AUTHOR("Etienne Boudreault-Pilon");
 MODULE_LICENSE("Dual BSD/GPL");
+
+// -- Constants --
+#define MINOR_NUM 0
 
 // -- Prototypes --
 static int __init usb_cam_init(void);
@@ -45,9 +50,11 @@ module_exit(usb_cam_exit);
 
 // -- Program stuctures --
 // My usb Device
-struct my_usb_cam_dev {
+struct usb_cam_dev {
     struct usb_device *usb_dev;
     struct usb_interface *usb_cam_intf;
+    struct urb *usb_can_urbs[5];
+    atomic_t urb_counter;
 };
 
 // fops
@@ -58,11 +65,12 @@ struct file_operations usb_cam_fops = {
 	.read           = usb_cam_read,
 	.write          = usb_cam_write,
 	.unlocked_ioctl = usb_cam_ioctl,
-}
+};
 
 // table
-static struct usb_device_id usb_cam_table [] = {
-	{USB_DEVICE(0x046d, 0x08cc)},
+static struct usb_device_id usb_cam_table[] = {
+	{ USB_DEVICE(0x046d, 0x08cc) },
+	{ USB_DEVICE(0x046d, 0x0994) },
 	{}
 };
 MODULE_DEVICE_TABLE(usb, usb_cam_table);
@@ -73,6 +81,13 @@ static struct usb_driver usb_cam_driver = {
 	.id_table   = usb_cam_table,
 	.probe      = usb_cam_probe,
 	.disconnect = usb_cam_disconnect,
+};
+
+// class
+static struct usb_class_driver usb_cam_class = {
+	.name       = "usb/usb_cam%d",
+	.fops       = &usb_cam_fops,
+	.minor_base = MINOR_NUM,
 };
 
 // -- Program Variables --
@@ -96,14 +111,89 @@ static void __exit usb_cam_exit(void)
 	printk(KERN_WARNING"USB CAM EXIT -- Deregistered\n");
 	usb_deregister(&usb_cam_driver);
 }
+
 static int usb_cam_probe (struct usb_interface *intf, const struct usb_device_id *devid)
 {
-	int retval = 0;
+	int retval = -1, streaming_intf_detected = 0;
+	struct usb_device *dev = interface_to_usbdev(intf);
+	struct usb_cam_dev *my_usb_dev = NULL;
+	
+	printk(KERN_WARNING"USB CAM PROBE\n");
+	// -- USB Device class/subclass detection --
+	if (intf->altsetting->desc.bInterfaceClass == CC_VIDEO)
+	{
+	    if (intf->altsetting->desc.bInterfaceSubClass == SC_VIDEOCONTROL)
+	    {
+	    	printk(KERN_WARNING"- video control interface detected\n");
+	    	return 0;	
+	    }
+	    if (intf->altsetting->desc.bInterfaceSubClass == SC_VIDEOSTREAMING)
+		{
+	    	printk(KERN_WARNING"- video streaming interface detected\n");
+	    	streaming_intf_detected = 1;
+		}
+	    else
+	    {
+	    	printk(KERN_ERR"- unsupported interface subclass detected, returning.\n");
+	    	return retval;
+	    }
+	}
+	else
+	{
+		printk(KERN_ERR"- unsupported interface class detected, returning.\n");
+		return retval;
+	}
+	if (streaming_intf_detected)
+	{
+		// -- Create USB Dev --
+		my_usb_dev = kmalloc(sizeof(struct usb_cam_dev), GFP_KERNEL);
+		if (my_usb_dev == NULL)
+		{
+			printk(KERN_ERR"- can't allocate usb device memory.");
+			return -ENOMEM;
+		}
+		// add ref to usb device struct count
+		my_usb_dev->usb_dev = usb_get_dev(dev);
+		
+		// link intf with usb device
+		usb_set_intfdata(intf, my_usb_dev);
+		
+		retval = usb_register_dev(intf, &usb_cam_class);
+		if (retval < 0)
+		{
+			printk(KERN_ERR"- can't get minor with USBCORE\n");
+			usb_set_intfdata(intf, NULL);
+			return retval;
+		}
+		else
+		{
+			printk(KERN_WARNING"- registered class with USBCORE\n");
+		}
+		atomic_set(&my_usb_dev->urb_counter, 1);
+		usb_set_interface(my_usb_dev->usb_dev, 1, 4);
+		printk(KERN_WARNING"- completed probe routine");
+	}
+	else
+	{
+		retval = -ENODEV;
+	}
 	return retval;
 }
 static void usb_cam_disconnect(struct usb_interface *intf)
 {
-	//TODO
+	struct usb_cam_dev *dev;
+	printk(KERN_WARNING"USB CAM DISCONNECT\n");
+	if (intf->altsetting->desc.bInterfaceClass == CC_VIDEO)
+	{
+		if (intf->altsetting->desc.bInterfaceSubClass == SC_VIDEOSTREAMING)
+		{
+			dev = usb_get_intfdata(intf);
+			usb_set_intfdata(intf, NULL);
+			usb_deregister_dev(intf, &usb_cam_class);
+			kfree(dev);
+			printk(KERN_WARNING"- disconnected and cleared resources\n");
+		}
+	}
 }
 static int usb_cam_open (struct inode *inode, struct file *filp)
 {
